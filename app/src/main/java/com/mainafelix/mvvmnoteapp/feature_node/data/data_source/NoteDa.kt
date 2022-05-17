aaac6760037851b227fdb4365f69549ea0875d1d
felixmaina <karani.maina2010@gmail.com>
2022-05-17 23:55:27 +0300

    create a query that selects  all  notes fro the table

--- app/src/main/java/com/mainafelix/mvvmnoteapp/feature_node/data/data_source/NoteDa.kt
+++ app/src/main/java/com/mainafelix/mvvmnoteapp/feature_node/data/data_source/NoteDa.kt
@@ -1,10 +1,12 @@
 package com.mainafelix.mvvmnoteapp.feature_node.data.data_source
 
 import androidx.room.Dao
+import androidx.room.Query
 import com.mainafelix.mvvmnoteapp.feature_node.domain_layer.model.Note
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface NoteDa {
+    @Query("SELECT * FROM note")
     fun getNotes(): Flow<List<Note>>
 }
