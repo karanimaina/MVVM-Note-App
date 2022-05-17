ff06d0d232f103bc5e6e52ea4a315543b37f8b04
felixmaina <karani.maina2010@gmail.com>
2022-05-17 23:36:26 +0300

    set the constructor values for the note class

--- app/src/main/java/com/mainafelix/mvvmnoteapp/feature_node/domain_layer/model/Note.kt
+++ app/src/main/java/com/mainafelix/mvvmnoteapp/feature_node/domain_layer/model/Note.kt
@@ -1,7 +1,14 @@
 package com.mainafelix.mvvmnoteapp.feature_node.domain_layer.model
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity
-class Note (){
+class Note (
+    val title:String,
+    val content :String,
+    val timeStamp: Long,
+    val  color:Int,
+    @PrimaryKey val id:Int?= null
+        ){
 }
