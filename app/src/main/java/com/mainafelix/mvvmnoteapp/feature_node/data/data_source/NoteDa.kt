b7adcc0fff40a2c95cfe866adb15c0250a6c3543
felixmaina <karani.maina2010@gmail.com>
2022-05-17 23:53:12 +0300

    create a function in the dao interface that will get all nodes

--- app/src/main/java/com/mainafelix/mvvmnoteapp/feature_node/data/data_source/NoteDa.kt
+++ app/src/main/java/com/mainafelix/mvvmnoteapp/feature_node/data/data_source/NoteDa.kt
@@ -1,7 +1,10 @@
 package com.mainafelix.mvvmnoteapp.feature_node.data.data_source
 
 import androidx.room.Dao
+import com.mainafelix.mvvmnoteapp.feature_node.domain_layer.model.Note
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface NoteDa {
+    fun getNotes(): Flow<List<Note>>
 }
