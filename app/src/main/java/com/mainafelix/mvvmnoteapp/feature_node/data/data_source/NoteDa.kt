215b2706ee74df7adf60c9ce216cde8ad635f701
felixmaina <karani.maina2010@gmail.com>
2022-05-18 09:00:49 +0300

    add dao functions that will perform crud operations on thee database

--- app/src/main/java/com/mainafelix/mvvmnoteapp/feature_node/data/data_source/NoteDa.kt
+++ app/src/main/java/com/mainafelix/mvvmnoteapp/feature_node/data/data_source/NoteDa.kt
@@ -1,14 +1,22 @@
 package com.mainafelix.mvvmnoteapp.feature_node.data.data_source
 
-import androidx.room.Dao
-import androidx.room.Query
+import androidx.room.*
 import com.mainafelix.mvvmnoteapp.feature_node.domain_layer.model.Note
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface NoteDa {
+    //returns a flow
     @Query("SELECT * FROM note")
     fun getNotes(): Flow<List<Note>>
-    @Query("SELECT * FROM note WHERE id =::id" )
+
+    @Query("SELECT * FROM note WHERE id =:id" )
     suspend fun getNoteById(id:Int):Note?
+// if we call the insert function with an id that already existing in the datanase  then it will just update the existing entry
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertNote(note : Note)
+    @Delete
+    suspend fun deleteNote(note:Note)
+
+
 }
