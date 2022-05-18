eb60a33a631fb4ea01a469580126268373b0edee
felixmaina <karani.maina2010@gmail.com>
2022-05-18 09:35:19 +0300

    implement the functions that are present in the noteDao class

--- app/src/main/java/com/mainafelix/mvvmnoteapp/feature_node/data/repository/NoteRepositoryImplementation.kt
+++ app/src/main/java/com/mainafelix/mvvmnoteapp/feature_node/data/repository/NoteRepositoryImplementation.kt
@@ -1,4 +1,26 @@
 package com.mainafelix.mvvmnoteapp.feature_node.data.repository
 
-class NoteRepositoryImplementation {
+import com.mainafelix.mvvmnoteapp.feature_node.data.data_source.NoteDao
+import com.mainafelix.mvvmnoteapp.feature_node.domain_layer.model.Note
+import kotlinx.coroutines.flow.Flow
+
+class NoteRepositoryImplementation(
+    private  val dao: NoteDao
+
+):NoteDao {
+    override fun getNotes(): Flow<List<Note>> {
+   return dao.getNotes()
+    }
+
+    override suspend fun getNoteById(id: Int): Note? {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun insertNote(note: Note) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteNote(note: Note) {
+        TODO("Not yet implemented")
+    }
 }
