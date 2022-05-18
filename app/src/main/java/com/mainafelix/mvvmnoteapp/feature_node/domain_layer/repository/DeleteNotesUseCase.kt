f6e1b954402e5a955d6ae40836be577add7c1153
felixmaina <karani.maina2010@gmail.com>
2022-05-18 19:07:42 +0300

    implement the repository delete function

--- app/src/main/java/com/mainafelix/mvvmnoteapp/feature_node/domain_layer/repository/DeleteNotesUseCase.kt
+++ app/src/main/java/com/mainafelix/mvvmnoteapp/feature_node/domain_layer/repository/DeleteNotesUseCase.kt
@@ -1,4 +1,11 @@
 package com.mainafelix.mvvmnoteapp.feature_node.domain_layer.repository
 
-class DeleteNotesUseCase {
+import com.mainafelix.mvvmnoteapp.feature_node.domain_layer.model.Note
+
+class DeleteNotesUseCase(
+    private  val repository: NodeRepository) {
+    suspend operator fun invoke(note: Note){
+        repository.deleteNode(note)
+
+    }
 }
