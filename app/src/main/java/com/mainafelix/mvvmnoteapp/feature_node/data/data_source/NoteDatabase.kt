26f7d2cb548ef0415d95be4055363d0fa48d0196
felixmaina <karani.maina2010@gmail.com>
2022-05-18 09:13:46 +0300

    add a database annotation wih the tables in place

--- app/src/main/java/com/mainafelix/mvvmnoteapp/feature_node/data/data_source/NoteDatabase.kt
+++ app/src/main/java/com/mainafelix/mvvmnoteapp/feature_node/data/data_source/NoteDatabase.kt
@@ -1,4 +1,15 @@
 package com.mainafelix.mvvmnoteapp.feature_node.data.data_source
 
- abstract class NoteDatabase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.mainafelix.mvvmnoteapp.feature_node.domain_layer.model.Note
+
+@Database(
+    // define the tables we have in the database
+    entities = [Note::class],
+    version = 1
+)
+
+abstract class NoteDatabase : RoomDatabase() {
+
 }
