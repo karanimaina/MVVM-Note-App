5ff6ead378344bdc10202ac68bb9457d981a0e10
felixmaina <karani.maina2010@gmail.com>
2022-05-18 21:00:47 +0300

    add the user interaction attributes

--- app/src/main/java/com/mainafelix/mvvmnoteapp/feature_node/presentation/notes/NoteEvents.kt
+++ app/src/main/java/com/mainafelix/mvvmnoteapp/feature_node/presentation/notes/NoteEvents.kt
@@ -1,5 +1,12 @@
 package com.mainafelix.mvvmnoteapp.feature_node.presentation.notes
 
-sealed class NoteEvents(){
-    data
+import com.mainafelix.mvvmnoteapp.feature_node.domain_layer.model.Note
+import com.mainafelix.mvvmnoteapp.feature_node.domain_layer.util.NoteOrder
+
+sealed class NoteEvents{
+    data class Order(val noteOrder: NoteOrder):NoteEvents()
+    data class DeleteNote(val note:Note):NoteEvents()
+    object RestoreNote:NoteEvents()
+    object ToggleOrderSection:NoteEvents()
+
 }
